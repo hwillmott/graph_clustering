d06e0cafb38dc05ffb7ad68eeb518de4b9230f74
Harriet Willmott <harriet.willmott@gmail.com>
2014-02-09 16:39:22 -0800

    clustering seems to work. want to modify when it stops and print out results

--- GraphClustering/src/Graph.java
+++ GraphClustering/src/Graph.java
@@ -1,35 +1,105 @@
-import java.util.ArrayList;
+import java.awt.Point;
 
 
 public class Graph 
 {
-	public ArrayList<Vertex> vertices;
+	public Vertex[] vertices;
+	public int[] clusterIndices;
 	public int[][] vertexProximityMatrix;
 	public int[][] clusterProximityMatrix;
+	public int m;
+	public int n;
 	
 	public Graph()
 	{
-		vertices = new ArrayList<Vertex>();
+		m = n = 0;
 	}
 	
 	public int indexOf(String name)
 	{
-		for(int i = 0; i < vertices.size(); i++)
+		for(int i = 0; i < vertices.length; i++)
 		{
-			if (vertices.get(i).name.equals(name)) return i;
+			if (vertices[i].name.equals(name)) return i;
 		}
 		return -1;
 	}
 	
 	public void printMatrix(int[][] matrix)
 	{
+		System.out.println("Printing Matrix");
+		System.out.println("matrix length: " + matrix.length);
+		System.out.println("matrix[0].length: " + matrix[0].length);
 		for(int i = 0; i < matrix.length; i++)
 		{
 			for(int j = 0; j < matrix[0].length; j++)
 			{
-				System.out.format("%03d ", matrix[i][j]);
+				System.out.format("%02d ", matrix[i][j]);
 			}
 			System.out.println();
+		}
+	}
+	
+	public void NewmanCluster(int iterations)
+	{
+		clusterIndices = new int[vertices.length];
+		for(int i = 0; i < vertices.length; i++) clusterIndices[i] = i;
+		
+		for(int j = 0; j < iterations; j++)
+		{
+			Point p = findMax(vertexProximityMatrix);
+			System.out.println("merging " + p.x + " and " + p.y);
+			mergeClusters(p.x, p.y);
+			//printMatrix(vertexProximityMatrix);
+		}
+	}
+	
+	public Point findMax(int[][] array)
+	{
+		int x, y, max;
+		x = y = max = -1;
+		
+		for(int i = 0; i < array.length; i++)
+		{
+			for(int j = 0; j < array[0].length; j++)
+			{
+				if(i != j)
+				{
+					if (max < array[i][j])
+					{
+						max = array[i][j];
+						x = i;
+						y = j;
+					}
+				}
+			}
+		}
+		if (max == 0) return new Point(-1,-1);
+		return new Point(x, y);
+	}
+	
+	public void mergeClusters(int index1, int index2)
+	{
+		int smallest = Math.min(index1, index2);
+		int largest = Math.max(index1, index2);
+		int newClusterIndex = clusterIndices[smallest];
+		int oldClusterIndex = clusterIndices[largest];
+		// update cluster array and the proximity matrix
+		for(int i = 0; i < clusterIndices.length; i++)
+		{
+			if (clusterIndices[i] == oldClusterIndex)
+			{
+				clusterIndices[i] = newClusterIndex;
+				for(int j = 0; j < vertexProximityMatrix[i].length; j++)
+				{
+					// take the maximum between the columns
+					vertexProximityMatrix[newClusterIndex][j] = vertexProximityMatrix[j][newClusterIndex] = Math.max(vertexProximityMatrix[newClusterIndex][j], vertexProximityMatrix[i][j]);
+				}
+				for(int j = 0; j < vertexProximityMatrix[i].length; j++)
+				{
+					vertexProximityMatrix[i][j] = vertexProximityMatrix[j][i] = -1;
+				}
+			}
+			
 		}
 	}
 }
--- GraphClustering/src/GraphHandler.java
+++ GraphClustering/src/GraphHandler.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 
@@ -6,128 +7,147 @@ public class GraphHandler
 {
 	public static void main(String[] args)
 	{
-		Graph G = new Graph();
-		parseGraphInput("./album-artist-genre.txt", G);
+		try {
+			PrintStream p = new PrintStream("output.txt");
+			System.setOut(p);
+			Graph G = new Graph();
+			parseGraphInput("./album-artist-genre.txt", G);
+			G.NewmanCluster(40);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void parseGraphInput(String filename, Graph G)
 	{
 		generateVertices(filename, G);
 		computeEdges(filename, G);
-		G.printMatrix(G.vertexProximityMatrix);
+		//G.printMatrix(G.vertexProximityMatrix);
+		System.out.println("vertices: " + G.n + " edges: " + G.m);
 	}
-	
+
 	public static void generateVertices(String filename, Graph G)
 	{
 		FileReader file = null;		  
-		  try {
-		    file = new FileReader(filename);
-		    BufferedReader reader = new BufferedReader(file);
-		    String line = "";
-		    String[] sections;
-		    String[] artists;
-		    String[] genres;
-		    Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
-		    while ((line = reader.readLine()) != null) {
-		      sections = line.split("\\*");
-		      artists = sections[1].split(",");
-		      genres = sections[2].split(",");
-		      for(int i = 0; i < artists.length; i++)
-		      {
-		    	  if (!hash.containsKey(artists[i]) && artists[i].length() != 0)
-		    	  {
-		    		  Vertex v = new Vertex(artists[i], VertexType.ARTIST);
-		    		  G.vertices.add(v);
-		    		  hash.put(artists[i], 1);
-		    		  System.out.println("Adding artist: " + artists[i]);
-		    	  }
-		      }
-		      for(int j = 0; j < genres.length; j++)
-	    	  {
-		    	  if (!hash.containsKey(genres[j]) && genres[j].length() != 0)
-		    	  {
-		    		  Vertex v = new Vertex(genres[j], VertexType.GENRE);
-		    		  G.vertices.add(v);
-		    		  hash.put(genres[j], 1);
-		    		  System.out.println("Adding genre: " + genres[j]);
-		    	  }
-	    	  }
-		    }
-		  } catch (Exception e) {
-		      throw new RuntimeException(e);
-		  } finally {
-		    if (file != null) {
-		      try {
-		        file.close();
-		      } catch (IOException e) {
-		      }
-		    }
-		  }
+		try {
+			file = new FileReader(filename);
+			BufferedReader reader = new BufferedReader(file);
+			String line = "";
+			String[] sections,artists,genres;
+			ArrayList<Vertex> vertices = new ArrayList<Vertex>();
+			Hashtable<String, Integer> hash = new Hashtable<String, Integer>();
+			while ((line = reader.readLine()) != null)
+			//for(int h = 0; h < 200; h++)
+			{
+				//line = reader.readLine();
+				sections = line.split("\\*");
+				artists = sections[1].split(",");
+				genres = sections[2].split(",");
+				for(int i = 0; i < artists.length; i++)
+				{
+					if (!hash.containsKey(artists[i]) && artists[i].length() != 0)
+					{
+						Vertex v = new Vertex(artists[i], VertexType.ARTIST);
+						vertices.add(v);
+						G.n++;
+						hash.put(artists[i], 1);
+						System.out.println("Adding artist: " + artists[i]);
+					}
+				}
+				for(int j = 0; j < genres.length; j++)
+				{
+					if (!hash.containsKey(genres[j]) && genres[j].length() != 0)
+					{
+						Vertex v = new Vertex(genres[j], VertexType.GENRE);
+						vertices.add(v);
+						G.n++;
+						hash.put(genres[j], 1);
+						System.out.println("Adding genre: " + genres[j]);
+					}
+				}
+			}
+			G.vertices = vertices.toArray(new Vertex[vertices.size()]);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (file != null) {
+				try {
+					file.close();
+				} catch (IOException e) {
+				}
+			}
+		}
 	}
-	
+
 	public static void computeEdges(String filename, Graph G)
 	{
-		G.vertexProximityMatrix = new int[G.vertices.size()][G.vertices.size()];
-		for(int i = 0; i < G.vertices.size(); i++)
+		G.vertexProximityMatrix = new int[G.vertices.length][G.vertices.length];
+		for(int i = 0; i < G.vertices.length; i++)
 		{
 			Arrays.fill(G.vertexProximityMatrix[i], 0);
 		}
 		FileReader file = null;		  
-		  try {
-		    file = new FileReader(filename);
-		    BufferedReader reader = new BufferedReader(file);
-		    String line = "";
-		    String[] sections,artists,genres;
-		    while ((line = reader.readLine()) != null) {
-		      sections = line.split("\\*");
-		      artists = sections[1].split(",");
-		      genres = sections[2].split(",");
-		      for(int i = 0; i < artists.length; i++)
-		      {
-			      for(int j = 0; j < genres.length; j++)
-		    	  {
-			    	  if(artists[i].length() != 0 && genres[j].length() != 0)
-			    	  {
-				    	  int a = G.indexOf(artists[i]);
-				    	  int g = G.indexOf(genres[j]);
-				    	  G.vertexProximityMatrix[a][g] += 1;
-			    	  }
-		    	  }
-		      }
-		      for(int i = 0; i < artists.length; i++)
-		      {
-		    	  for(int j = 0; j < artists.length; j++)
-		    	  {
-		    		  if(artists[i].length() != 0 && artists[j].length() != 0)
-			    	  {
-				    	  int a = G.indexOf(artists[i]);
-				    	  int g = G.indexOf(artists[j]);
-				    	  G.vertexProximityMatrix[a][g] += 1;
-			    	  }
-		    	  }
-		      }
-		      for(int i = 0; i < genres.length; i++)
-	    	  {
-		    	  for(int j = 0; j < genres.length; j++)
-		    	  {
-		    		  if(genres[i].length() != 0 && genres[j].length() != 0)
-			    	  {
-				    	  int a = G.indexOf(genres[i]);
-				    	  int g = G.indexOf(genres[j]);
-				    	  G.vertexProximityMatrix[a][g] += 1;
-			    	  }
-		    	  }
-	    	  }
-		    }
-		  } catch (Exception e) {
-		      throw new RuntimeException(e);
-		  } finally {
-		    if (file != null) {
-		      try {
-		        file.close();
-		      } catch (IOException e) {
-		      }
-		    }
-		  }
+		try {
+			file = new FileReader(filename);
+			BufferedReader reader = new BufferedReader(file);
+			String line = "";
+			String[] sections,artists,genres;
+			while ((line = reader.readLine()) != null) 
+			//for(int h = 0; h < 200; h++)
+			{
+				//line = reader.readLine();
+				sections = line.split("\\*");
+				artists = sections[1].split(",");
+				genres = sections[2].split(",");
+				for(int i = 0; i < artists.length; i++)
+				{
+					for(int j = 0; j < genres.length; j++)
+					{
+						if(artists[i].length() != 0 && genres[j].length() != 0)
+						{
+							int a = G.indexOf(artists[i]);
+							int g = G.indexOf(genres[j]);
+							G.vertexProximityMatrix[a][g] += 1;
+							G.m++;
+						}
+					}
+				}
+				for(int i = 0; i < artists.length; i++)
+				{
+					for(int j = 0; j < artists.length; j++)
+					{
+						if(artists[i].length() != 0 && artists[j].length() != 0)
+						{
+							int a = G.indexOf(artists[i]);
+							int g = G.indexOf(artists[j]);
+							G.vertexProximityMatrix[a][g] += 1;
+							G.m++;
+						}
+					}
+				}
+				for(int i = 0; i < genres.length; i++)
+				{
+					for(int j = 0; j < genres.length; j++)
+					{
+						if(genres[i].length() != 0 && genres[j].length() != 0)
+						{
+							int a = G.indexOf(genres[i]);
+							int g = G.indexOf(genres[j]);
+							G.vertexProximityMatrix[a][g] += 1;
+							G.m++;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (file != null) {
+				try {
+					file.close();
+				} catch (IOException e) {
+				}
+			}
+		}
 	}
 }
