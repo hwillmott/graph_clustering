75d42f1865263a0a1bf409f819dc22e9d7e65d77
Harriet Willmott <harriet.willmott@gmail.com>
2014-03-19 13:55:10 -0700

    removed obsolete files

--- GraphClustering/src/ArtistVertex.java
+++
@@ -1,9 +0,0 @@
-
-public class ArtistVertex extends Vertex
-{
-	public ArtistVertex(){}
-	public ArtistVertex(String name)
-	{
-		this.name = name;
-	}
-}
--- GraphClustering/src/GenreVertex.java
+++
@@ -1,9 +0,0 @@
-
-public class GenreVertex extends Vertex
-{
-	public GenreVertex(){}
-	public GenreVertex(String name)
-	{
-		this.name = name;
-	}
-}
